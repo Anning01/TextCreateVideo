78e651a12d4ec430074c0608b22fc3360e0f303b
anning <864399407@qq.com>
2023-08-19 10:19:34 +0800

    更新加密

--- admin/views.py
+++ admin/views.py
@@ -5,4 +5,20 @@
 # @time:2023/08/04 20:52
 # @file:views.py
 
-from views.cp310-win_amd64.pyd import  *
+import sys
+
+if sys.platform.startswith('linux'):
+    print('当前系统为 Linux')
+    __import__('views.cpython-310-darwin.so')
+elif sys.platform.startswith('win'):
+    print('当前系统为 Windows')
+    __import__('views.cp310-win_amd64.pydimport.pyd')
+
+elif sys.platform.startswith('darwin'):
+    print('当前系统为 macOS')
+    __import__('views.cpython-310-darwin.so')
+
+else:
+    print('无法识别当前系统')
+    raise Exception('无法识别当前系统')
+
